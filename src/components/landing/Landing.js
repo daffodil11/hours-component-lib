c9da1671db1642696423800154349e23dacaf0c5
Simon Davey <simonr.davey@gmail.com>
2019-04-16 12:08:25 +0100

    Working on making slides with image backgrounds.

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Slide} from '../../styles/StyledComponents';
+import TestImage from '../../assets/logHours.png';
+
+function Landing(slides) {
+    return (
+        <Slide/> 
+    );
+}
