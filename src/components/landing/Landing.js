8c2135fa753976e1e275602a435970726cc55ac0
Simon Davey <simonr.davey@gmail.com>
2019-04-16 12:37:47 +0100

    Filled out Landing component.

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -3,9 +3,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Slide} from '../../styles/StyledComponents';
 import TestImage from '../../assets/logHours.png';
+import Content from './Content';
 
 function Landing(slides) {
     return (
-        <Slide/> 
+        <Slide image={TestImage}>
+          <Content content={(({image, ...elems}) => ({...elems}))(slides[0])} />
+        </Slide>
     );
 }
+
+Landing.propTypes = {
+    slides: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        image: PropTypes.string.isRequired,
+        header: PropTypes.string.isRequired,
+        body: PropTypes.string.isRequired
+    }))
+};
